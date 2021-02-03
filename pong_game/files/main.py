9879c744063155f81de5786c792cf665390fbb07
Peter Kovac <pojzinko8@gmail.com>
2021-02-03 19:28:39 +0100

    commit

--- pong_game/files/main.py
+++ pong_game/files/main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
+  
 screen = pygame.display.set_mode((640, 480))
 
 running = 1
@@ -9,11 +10,13 @@ running = 1
 while running:
   for event in pygame.event.get():
     if event.type == QUIT:
-      running = 0
+        running = 0
     else:
-      print (event)
+        pass
 
   screen.fill((120, 120, 120))
+  pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(100, 100, 200, 200))
   pygame.display.flip()
+
 
 pygame.quit()
